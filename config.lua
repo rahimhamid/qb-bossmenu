9447cd666740f19e25f04b59105b282daa412738
rahimhamid <78023684+rahimhamid@users.noreply.github.com>
2021-10-09 21:02:21 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ Config.Jobs = {
     ['ambulance'] = vector3(303.88, -600.77, 43.28),
     ['realestate'] = vector3(-124.786, -641.486, 167.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
+    ['mancustoms'] = vector3(-99.18, -1793.04, 32.2),
     ['cardealer'] = vector3(-53.95, 73.89, 71.89),
     ['mechanic'] = vector3(-338.71, -157.8, 44.59)
-    ['mancustoms'] = vector3(-96.33, -1795.2, 32.2)
 }
